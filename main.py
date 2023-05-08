f23349d1db29761296e1c26c6d3cd5de61abd1a5
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-05-08 15:11:09 +0400

    random

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import requests
+import random
 import time
 import logging
 
@@ -23,32 +24,39 @@ meme_api_links = os.environ.get("MEME_API").split()
 chat_ids = [int(ch) if id_pattern.search(ch) else ch for ch in os.environ.get('CHAT_ID', '0').split()]
 owner_id = int(os.environ.get("OWNER_ID"))
 time_gap = int(os.environ.get("TIME_GAP"))
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging_level = int(os.environ.get("LOGGING_LEVEL", logging.INFO))
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging_level)
+logger = logging.getLogger(__name__)
 
 with app:
     app.send_message(owner_id, "Bot started!")
 
 while True:
-    for meme_api_link in meme_api_links:
-        try:
-            response = requests.get(meme_api_link)
-            meme_data = response.json()
-            meme_title = meme_data["title"]
-            meme_url = meme_data["url"]
-            with app:
-                for chat_id in chat_ids:
-                    sent_meme = app.send_photo(chat_id, photo=meme_url, caption=meme_title)
-                    post_link = sent_meme.link
-                    app.send_message(owner_id, f'Meme sent to chat ID {chat_id}:\n<a href="{post_link}">POST LINK</a>', disable_web_page_preview=True, parse_mode='html')
+    try:
+        meme_api_link = random.choice(meme_api_links)
+        response = requests.get(meme_api_link)
+        response.raise_for_status()
 
-        except requests.exceptions.ConnectionError as e:
-            logging.error(f'Connection error occurred while accessing {meme_api_link}: {str(e)}')
-        except KeyError as e:
-            logging.error(f'Key error occurred while processing the meme data from {meme_api_link}: {str(e)}')
-        except Exception as e:
-            logging.error(f"Error occurred while processing the meme data from {meme_api_link}: {str(e)}")
+        meme_data = response.json()
+        meme_title = meme_data.get("title")
+        meme_url = meme_data.get("url")
+
+        with app:
+            for chat_id in chat_ids:
+                sent_meme = app.send_photo(chat_id, photo=meme_url, caption=meme_title)
+                post_link = sent_meme.link
+                app.send_message(owner_id, f"Meme sent to chat ID {chat_id}:\n{post_link}")
+
+    except requests.exceptions.HTTPError as e:
+        logger.error(f"HTTP error occurred: {str(e)}")
+    except requests.exceptions.ConnectionError as e:
+        logger.error(f"Error connecting to API: {str(e)}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error occurred: {str(e)}")
+    except Exception as e:
+        logger.error(f"Unexpected error occurred: {str(e)}")
 
     time.sleep(time_gap)
