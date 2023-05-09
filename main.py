d63d4edab5e8fe078c267dd2c7d664ce516642ac
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-05-09 11:33:42 +0400

    postgres

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import re
 import requests
 import time
 import random
-import sqlite3
-import mysql.connector
+import psycopg2
 from pyrogram import Client, enums
 
 id_pattern = re.compile(r'^.\d+$')
-
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -20,20 +18,22 @@ def is_enabled(value, default):
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
-mysql_url = os.environ.get("MYSQL_URL")
-meme_apis = [os.environ.get(f"MEME_API{i}") for i in range(2, 6)]
+meme_api_links = [os.environ.get("MEME_API"), os.environ.get("MEME_API2"), os.environ.get("MEME_API3"), os.environ.get("MEME_API4"), os.environ.get("MEME_API5")]
+meme_api_links = [link for link in meme_api_links if link]  # Remove empty links
 chat_ids = [int(ch.strip("'")) for ch in os.environ.get('CHAT_ID', '0').split()]
 owner_id = int(os.environ.get("OWNER_ID"))
 time_gap = int(os.environ.get("TIME_GAP"))
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-conn = mysql.connector.connect(host=mysql_url, user='user', password='password', database='sent_memes_db', autocommit=True)
+conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS sent_memes (url text primary key)''')
+conn.commit()
 
 with app:
     app.send_message(owner_id, "Bot started!")
+
 
 def send_meme(chat_id, meme_url, meme_title, meme_api_link):
     try:
@@ -44,13 +44,7 @@ def send_meme(chat_id, meme_url, meme_title, meme_api_link):
         app.send_message(owner_id, f"Error occurred while sending meme to chat ID {chat_id}: {str(e)} Link: \n\n{meme_api_link}")
 
 while True:
-    meme_api_link = random.choice([m for m in meme_apis if m])  # Randomly select an API from those with values
-    if not meme_api_link:
-        with app:
-            app.send_message(owner_id, "No valid MEME_API found. Check your environment variables.")
-        time.sleep(time_gap)
-        continue
-        
+    meme_api_link = random.choice(meme_api_links) if meme_api_links else os.environ.get("MEME_API")
     retry_count = 0
     max_retry_count = 5
     while retry_count < max_retry_count:
@@ -67,6 +61,7 @@ while True:
                 for chat_id in chat_ids:
                     send_meme(chat_id, meme_url, meme_title, meme_api_link)
             c.execute('INSERT INTO sent_memes (url) VALUES (%s)', (meme_url,))
+            conn.commit()
             break  
         except requests.exceptions.HTTPError as e:
             with app:
@@ -82,3 +77,4 @@ while True:
             app.send_message(owner_id, f"Max retry count exceeded for API {meme_api_link}")
 
     time.sleep(time_gap)
+    
